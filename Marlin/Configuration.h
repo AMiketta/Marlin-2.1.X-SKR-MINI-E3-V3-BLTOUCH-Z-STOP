5333d1bd888d41aabadced52e4bde6898c5d98d0
Andreas Miketta <andreas.miketta@hmmh.de>
2023-01-18 07:44:20 +0100

    BL TOUCH DELAY New Nozzle offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1488,7 +1488,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 35, 1, -3.04 } // 3DWORK PROBE
+#define NOZZLE_TO_PROBE_OFFSET { 35, 1, -3.02 } // 3DWORK PROBE
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
